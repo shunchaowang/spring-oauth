929adf149b981e526f1beea4f4da5142c1acc22c
Shunchao Wang <wang.2799@osu.edu>
2019-01-30 16:20:36 -0500

    Added external client details from mysql database.

--- bmi-oauth2-auth/src/main/java/edu/osumc/bmi/oauth2/auth/BmiOauth2AuthBeanHolder.java
+++ bmi-oauth2-auth/src/main/java/edu/osumc/bmi/oauth2/auth/BmiOauth2AuthBeanHolder.java
@@ -1,7 +1,15 @@
 package edu.osumc.bmi.oauth2.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BmiOauth2AuthBeanHolder {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- bmi-oauth2-auth/src/main/java/edu/osumc/bmi/oauth2/auth/user/UserDetailsServiceImpl.java
+++ bmi-oauth2-auth/src/main/java/edu/osumc/bmi/oauth2/auth/user/UserDetailsServiceImpl.java
@@ -1,26 +1,34 @@
 package edu.osumc.bmi.oauth2.auth.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
-    /**
-     * Locates the user based on the username. In the actual implementation, the search
-     * may possibly be case sensitive, or case insensitive depending on how the
-     * implementation instance is configured. In this case, the <code>UserDetails</code>
-     * object that comes back may have a username that is of a different case than what
-     * was actually requested..
-     *
-     * @param username the username identifying the user whose data is required.
-     * @return a fully populated user record (never <code>null</code>)
-     * @throws UsernameNotFoundException if the user could not be found or the user has no
-     *                                   GrantedAuthority
-     */
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
+
+  @Autowired private PasswordEncoder passwordEncoder;
+  /**
+   * Locates the user based on the username. In the actual implementation, the search may possibly
+   * be case sensitive, or case insensitive depending on how the implementation instance is
+   * configured. In this case, the <code>UserDetails</code> object that comes back may have a
+   * username that is of a different case than what was actually requested..
+   *
+   * @param username the username identifying the user whose data is required.
+   * @return a fully populated user record (never <code>null</code>)
+   * @throws UsernameNotFoundException if the user could not be found or the user has no
+   *     GrantedAuthority
+   */
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return new User(
+        username,
+        passwordEncoder.encode("123456"),
+        AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+  }
 }
