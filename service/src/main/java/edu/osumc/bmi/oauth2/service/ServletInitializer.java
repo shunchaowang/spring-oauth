a7c5d53ef8acde01d4f6fcc6f72999671298465e
Shunchao Wang <wang.2799@osu.edu>
2019-01-31 10:19:17 -0500

    Added restful service for admin ap.

---
+++ service/src/main/java/edu/osumc/bmi/oauth2/service/ServletInitializer.java
@@ -0,0 +1,14 @@
+package edu.osumc.bmi.oauth2.service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServiceApplication.class);
+	}
+
+}
+
