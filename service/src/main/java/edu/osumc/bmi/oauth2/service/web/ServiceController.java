120cce4f3aba6d38cd84b6f8aaaeb9d54b415398
Shunchao Wang <wang.2799@osu.edu>
2019-03-01 09:51:22 -0500

    Finished service oauth2 redirect call.

--- service/src/main/java/edu/osumc/bmi/oauth2/service/web/ServiceController.java
+++ service/src/main/java/edu/osumc/bmi/oauth2/service/web/ServiceController.java
@@ -10,7 +10,6 @@ import org.springframework.util.MultiValueMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
