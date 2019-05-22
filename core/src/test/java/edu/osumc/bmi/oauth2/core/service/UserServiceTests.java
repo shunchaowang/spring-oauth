15ec2ea00ee7a7afa7247b940e31ad1618b07ed3
Shunchao Wang <wang.2799@osu.edu>
2019-05-22 09:47:25 -0400

    Fixed user service tests.

--- core/src/test/java/edu/osumc/bmi/oauth2/core/service/UserServiceTests.java
+++ core/src/test/java/edu/osumc/bmi/oauth2/core/service/UserServiceTests.java
@@ -1,5 +1,8 @@
 package edu.osumc.bmi.oauth2.core.service;
 
+import edu.osumc.bmi.oauth2.core.Constants;
+import edu.osumc.bmi.oauth2.core.domain.Client;
+import edu.osumc.bmi.oauth2.core.domain.Role;
 import edu.osumc.bmi.oauth2.core.domain.User;
 import edu.osumc.bmi.oauth2.core.repository.ClientRepository;
 import edu.osumc.bmi.oauth2.core.repository.RoleRepository;
@@ -13,20 +16,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTests {
 
   @Autowired private UserService userService;
-  @MockBean private UserRepository userRepository;
   @MockBean private ClientRepository clientRepository;
   @MockBean private RoleRepository roleRepository;
+  @MockBean private UserRepository userRepository;
 
   @Test
   public void testCreateOAuth2Admin() {
+    // mock role
+    when(roleRepository.getOne(Constants.BMI_OAUTH2_SERVICE_ROLE_ADMIN_ID)).thenReturn(new Role());
+
+    // mock client
+    when(clientRepository.getOne(Constants.BMI_OAUTH2_SERVICE_ID)).thenReturn(new Client());
+
     User user = User.builder.username("john").password("john").active(true).build();
 
     user = userService.createOAuth2Admin(user);
+
+    assertThat(user != null);
+  }
+
+
+  @Test
+  public void testRegisterOAuth2User() {
+// mock role
+    when(roleRepository.getOne(Constants.BMI_OAUTH2_SERVICE_ROLE_USER_ID)).thenReturn(new Role());
+
+    // mock client
+    when(clientRepository.getOne(Constants.BMI_OAUTH2_SERVICE_ID)).thenReturn(new Client());
+
+    User user = User.builder.username("john").password("john").active(true).build();
+
+    user = userService.registerOAuth2User(user);
 
     assertThat(user != null);
   }
