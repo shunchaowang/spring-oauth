992955dd111d2f9d0038bb94687c9b7e9f4b47dc
Shunchao Wang <wang.2799@osu.edu>
2019-10-21 08:51:13 -0400

    Removed ClientDetail.

--- core/src/main/java/edu/osumc/bmi/oauth2/core/repository/ClientRepository.java
+++ core/src/main/java/edu/osumc/bmi/oauth2/core/repository/ClientRepository.java
@@ -1,10 +1,6 @@
 package edu.osumc.bmi.oauth2.core.repository;
 
 import edu.osumc.bmi.oauth2.core.domain.Client;
-import edu.osumc.bmi.oauth2.core.dto.ClientDetail;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,21 +11,4 @@ import java.util.Optional;
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
 
   Optional<Client> findByOAuth2ClientId(String oauth2ClientId) throws EntityNotFoundException;
-
-  @Query(
-      "select new edu.osumc.bmi.oauth2.core.dto.ClientDetail(client.name, client.active, "
-          + "oAuthClientDetail.clientId, oAuthClientDetail.authorizedGrantTypes, oAuthClientDetail.webServerRedirectUri, "
-          + "oAuthClientDetail.accessTokenValidity, oAuthClientDetail.refreshTokenValidity, oAuthClientDetail.autoApprove) "
-          + "from OAuthClientDetail oAuthClientDetail "
-          + "left join Client client on oAuthClientDetail.clientId = client.oAuth2ClientId "
-          + "where oAuthClientDetail.clientId = ?1")
-  Optional<ClientDetail> findClientDetailByClientId(String clientId);
-
-  @Query(
-      "select new edu.osumc.bmi.oauth2.core.dto.ClientDetail(client.name, client.active, "
-          + "oAuthClientDetail.clientId, oAuthClientDetail.authorizedGrantTypes, oAuthClientDetail.webServerRedirectUri, "
-          + "oAuthClientDetail.accessTokenValidity, oAuthClientDetail.refreshTokenValidity, oAuthClientDetail.autoApprove) "
-          + "from OAuthClientDetail oAuthClientDetail "
-          + "left join Client client on oAuthClientDetail.clientId = client.oAuth2ClientId")
-  Page<ClientDetail> findAllClientDetails(Pageable pageable);
 }
